a21310898468a91cf08f5bdfd9d2916f24675883
Dania Emmanuel <111269312+Godson-dan@users.noreply.github.com>
2022-10-18 21:07:51 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
+
 /**
  * _printf - a function that prints formouted output to stdout
+ *
  * @format: the string that contains any format specifier
+ *
  * Return: returns the number of printed characters
  */
 int _printf(const char *format, ...)
@@ -12,9 +15,11 @@ int _printf(const char *format, ...)
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
+
 	if (format == NULL)
 		return (-1);
 	va_start(list, format);
+
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
@@ -39,19 +44,22 @@ int _printf(const char *format, ...)
 			printed_chars += printed;
 		}
 	}
+
 	print_buffer(buffer, &buff_ind);
-	va_end(list);
-        return (printed_chars);
+        va_end(list);
+	return (printed_chars);
 }
+
 /**
  * print_buffer - Prints the contents of the buffer if it exist
+ *
  * @buffer: Array of chars
+ *
  * @buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
-
 	*buff_ind = 0;
-} 
+}
