68b1213fb6f1c4e81b7c641c861dd552b30cdfed
mirian320 <sylvamirian54@gmail.com>
2022-10-19 14:30:09 -0700

    converter

--- converter.c
+++ converter.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
-/**
+/*
  * convert - converts number and base into string
  * @num: input number
  * @base: input base
  * @lowercase: flag if hexa values need to be lowercase
  * Return: result string
- */
+*/
 
-char *convert(unsigned long int num, int base, int lowercase)
+
+char *convert(unsigned long int num, int base, int lowercase);
 {
 static char *rep;
 static char buffer[50];
@@ -23,6 +24,5 @@ do {
 *--ptr = rep[num % base];
 num /= base;
 } while (num != 0);
-
 return (ptr);
 }
